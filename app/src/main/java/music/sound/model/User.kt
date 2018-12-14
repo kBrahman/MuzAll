3cc1bae3f7088625176f52b0741da9365437c587
Kairat Doshekenov <kayrat@bk.ru>
2018-12-14 17:11:17 +0600

    fix NotSerializableException

--- app/src/main/java/music/sound/model/User.kt
+++ app/src/main/java/music/sound/model/User.kt
@@ -1,8 +1,7 @@
 package music.sound.model
 
 import android.support.annotation.Keep
+import java.io.Serializable
 
 @Keep
-class User(val avatar_url: String) {
-
-}
+class User(val avatar_url: String) : Serializable
