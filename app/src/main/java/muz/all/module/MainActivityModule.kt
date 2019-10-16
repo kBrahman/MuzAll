b79bcd8db70e539a17c674b517d9144fc5c34e31
Kairat Doshekenov <kayrat@bk.ru>
2019-10-17 03:12:14 +0600

    add id

--- app/src/main/java/muz/all/module/MainActivityModule.kt
+++ app/src/main/java/muz/all/module/MainActivityModule.kt
@@ -45,6 +45,7 @@ class MainActivityModule {
     fun provideIdIterator(): Iterator<String> {
         if (iterator == null) {
             val list = listOf(
+                BuildConfig.CLIENT_ID_21,
                 BuildConfig.CLIENT_ID_20,
                 BuildConfig.CLIENT_ID_1,
                 BuildConfig.CLIENT_ID_2,
