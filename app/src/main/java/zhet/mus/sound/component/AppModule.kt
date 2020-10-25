097b6b127040c35531b010b59532971f7d485e63
Kairat Doshekenov <kayrat@bk.ru>
2020-10-25 16:16:58 +0600

    format

--- app/src/main/java/zhet/mus/sound/component/AppModule.kt
+++ app/src/main/java/zhet/mus/sound/component/AppModule.kt
@@ -3,12 +3,11 @@ package zhet.mus.sound.component
 import dagger.Module
 import dagger.android.AndroidInjector
 import dagger.android.ContributesAndroidInjector
-import dagger.android.DaggerActivity
 import dagger.android.support.DaggerAppCompatActivity
 import zhet.mus.sound.activity.MainActivity
 
 @Module
-interface AppModule:AndroidInjector<DaggerAppCompatActivity> {
+interface AppModule : AndroidInjector<DaggerAppCompatActivity> {
 
     @ContributesAndroidInjector
     fun mainActivity(): MainActivity
