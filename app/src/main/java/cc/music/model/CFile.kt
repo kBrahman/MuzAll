5c5fe732ec604a7111d8f7b669176f5d5a446b7c
Kairat Doshekenov <kayrat@bk.ru>
2021-01-04 01:21:45 +0600

    update build tools v, bug fix

--- app/src/main/java/cc/music/model/CFile.kt
+++ app/src/main/java/cc/music/model/CFile.kt
@@ -1,6 +1,7 @@
 package cc.music.model
 
 import androidx.annotation.Keep
+import java.io.Serializable
 
 @Keep
-data class CFile(val download_url: String, val file_format_info: FileFormatInfo)
+data class CFile(val download_url: String, val file_format_info: FileFormatInfo) : Serializable
