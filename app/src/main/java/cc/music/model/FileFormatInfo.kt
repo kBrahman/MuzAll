70c60e3d8cb2a2fa93b8b47b56ff4a5bb94d7a02
Kairat Doshekenov <kayrat@bk.ru>
2021-01-08 03:30:34 +0600

    bug fix

--- app/src/main/java/cc/music/model/FileFormatInfo.kt
+++ app/src/main/java/cc/music/model/FileFormatInfo.kt
@@ -1,6 +1,7 @@
 package cc.music.model
 
 import androidx.annotation.Keep
+import java.io.Serializable
 
 @Keep
-data class FileFormatInfo(val ps: String)
+data class FileFormatInfo(val ps: String) : Serializable
